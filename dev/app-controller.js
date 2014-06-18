5902d66474e1961bb364dc06fb8b294b26164806
Chris Sharon <chris@xdoji.com>
2014-06-18 15:57:29 -0400

    remove dev from repo

--- dev/app-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('marvelapp')
-  .controller('appCtrl', function ($scope, xdMoment) {
-    $scope.appName = 'Comics';
-    $scope.company = 'XDoji, LLC.';
-    $scope.currentYear = xdMoment().year();
-
-
-  });
